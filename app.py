76395a4bf5eddc286de664763064784ba4bc6b77
keizell <97519119+keizell@users.noreply.github.com>
2023-07-01 01:06:59 +0000

    v2

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "helloo"
+  return render_template("home.html")
 app.run(host='0.0.0.0', debug=True)
